b97cbce4afc5d95d325111590c469941bf0a620c
fishagbalo <fishagbalo@gmail.com>
2024-07-02 22:13:37 +0800

    adding name list and other components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function NameList(){
+  const [list, setList] = useState(["Jack", "Nard", "John"])
+  const [name, setName] = useState("");
+  const addName = () =>{
+   setList([...list, name]) 
+  }
+  return(
+    <div>
+      <ul>
+        {list.map((name)=> (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <input type="text" name="" value={name} id="" onChange={(e) => setName(e.target.value)}/>
+      <button onClick={addName}>Add Name</button>
+    </div> 
+  )
+}
+
+function Counter() {
   const [count, setCount] = useState(0)
 
   function addOne(){
@@ -13,6 +32,16 @@ function App() {
     <>
       <button onClick={addOne}>Count = {count}</button>
     </>
+  )
+}
+
+function App(){
+  return (
+    <div>
+      <Counter/>
+  
+      <NameList/>
+    </div>
   )
 }
 
